54abd168f8054e81dc682895c77dbda3ccb51655
bhatia-di <bhatia.di@northeastern.edu>
2022-12-09 03:13:41 -0500

    added boostrap and fontawesome

--- shopify-frontend/src/core/Menu.js
+++ shopify-frontend/src/core/Menu.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
 import { itemTotal } from "./cartHelpers";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShop, faHome, faShoppingBasket, faShoppingCart, faSignIn, faSignOut, faSign } from '@fortawesome/free-solid-svg-icons'
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -13,38 +15,40 @@ const isActive = (history, path) => {
 
 const Menu = ({ history }) => (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light float-right">
-            <a className="navbar-brand float-left shopify-header" href="#"><i><b>Shopify</b></i></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <nav className="navbar navbar-expand-lg navbar-light">
 
             <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
+                    <li className="nav-item active ">
                         <Link
-                            className="nav-link"
+                            className="nav-link border-right-divider"
                             style={isActive(history, "/")}
                             to="/"
                         >
+                            <FontAwesomeIcon icon={faHome} /> &nbsp;
+
                             HOME
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item ">
                         <Link
-                            className="nav-link"
+                            className="nav-link border-right-divider"
                             style={isActive(history, "/shop")}
                             to="/shop"
                         >
+                            <FontAwesomeIcon icon={faShoppingBasket} /> &nbsp;
+
                             SHOP
                         </Link>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item ">
                         <Link
-                            className="nav-link"
+                            className="nav-link border-right-divider"
                             style={isActive(history, "/cart")}
                             to="/cart"
                         >
+                            <FontAwesomeIcon icon={faShoppingCart} /> &nbsp;
+
                             CART {" "}
                             <sup>
                                 <small className="cart-badge">{itemTotal()}</small>
@@ -56,9 +60,9 @@ const Menu = ({ history }) => (
 
                     {
                     isAuthenticated() && isAuthenticated().user.role === 0 && (
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <Link
-                                className="nav-link"
+                                className="nav-link border-right-divider"
                                 style={isActive(history, "/user/dashboard")}
                                 to="/user/dashboard"
                             >
@@ -70,9 +74,9 @@ const Menu = ({ history }) => (
 
 
                     {isAuthenticated() && isAuthenticated().user.role === 1 && (
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <Link
-                                className="nav-link"
+                                className="nav-link border-right-divider"
                                 style={isActive(history, "/admin/dashboard")}
                                 to="/admin/dashboard"
                             >
@@ -83,22 +87,26 @@ const Menu = ({ history }) => (
 
                     {!isAuthenticated() && (
                         <Fragment>
-                            <li className="nav-item">
+                            <li className="nav-item ">
                                 <Link
-                                    className="nav-link"
+                                    className="nav-link border-right-divider"
                                     style={isActive(history, "/signin")}
                                     to="/signin"
                                 >
+                                <FontAwesomeIcon icon={faSignIn} /> &nbsp;
+
                                     SIGN IN
                                 </Link>
                             </li>
 
-                            <li className="nav-item">
+                            <li className="nav-item ">
                                 <Link
-                                    className="nav-link"
+                                    className="nav-link border-right-divider"
                                     style={isActive(history, "/signup")}
                                     to="/signup"
                                 >
+                                <FontAwesomeIcon icon={faSign} /> &nbsp;
+
                                     SIGN UP
                                 </Link>
                             </li>
@@ -106,9 +114,9 @@ const Menu = ({ history }) => (
                     )}
 
                     {isAuthenticated() && (
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <span
-                                className="nav-link"
+                                className="nav-link border-right-divider"
                                 style={{ cursor: "pointer", color: "#ffffff" }}
                                 onClick={() =>
                                     signout(() => {
@@ -116,6 +124,8 @@ const Menu = ({ history }) => (
                                     })
                                 }
                             >
+                                <FontAwesomeIcon icon={faSignOut} /> &nbsp;
+
                                 SIGN OUT
                             </span>
                         </li>
@@ -124,6 +134,13 @@ const Menu = ({ history }) => (
 
                 </ul>
             </div>
+            <a className="navbar-brand float-left shopify-header" href="#"><i><b>
+                <FontAwesomeIcon icon={faShop} />
+                Shopify</b></i></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
         </nav>
 
     </div>
