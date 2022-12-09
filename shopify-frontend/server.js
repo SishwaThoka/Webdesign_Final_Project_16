6f9b838e87007345534a9a31380eddcb83e00ede
Rohan Teja V <rohantejav@gmail.com>
2022-12-09 00:01:08 -0500

    fix bugs, update env, server

--- shopify-frontend/server.js
+++ shopify-frontend/server.js
@@ -3,6 +3,10 @@ const compression = require('compression');
 const path = require('path');
 const app = express();
 
+/**
+ * @author Rohan Teja
+ */
+
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
